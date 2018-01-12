6551a33bb199d4ed98b70b7ed6e7a6b7c55f83ed
Matt Ross <amsross@gmail.com>
2018-01-11 23:08:51 -0500

    fix: correct use of weightedChoice. more branches

--- lib.js
+++ lib.js
@@ -6,12 +6,15 @@ const aOrAn = match()
   .case(/^[aeiou]/, x => 'an ' + x)
   .case(/.*/, x => 'a ' + x)
 
-const people = declare(() => weightedChoice(
+const people = declare(() => weightedChoice([
   [0.1, 'the alt right'],
   [0.1, 'deplorables'],
   [0.1, 'the illuminati'],
-  [0.7, sepBy(' ',
+  [0.1, 'millenials'],
+  [0.6, sepBy(' ',
     choice(
+      'mole',
+      'snake',
       'immitation',
       'fake',
       'albino',
@@ -20,7 +23,7 @@ const people = declare(() => weightedChoice(
       'black',
       'disenfranchised'),
     'people')]
-))
+]))
 
 const company = capitalizeFirst(declare(self => seq(
   opt(self, 0.1),
@@ -56,13 +59,15 @@ const industryDemographic = declare(() => choice(
 
 const customerIndustry = declare(() => choice(
   'stolen identities',
-  'NERF',
   'Trump\'s cabinet',
   'the womb',
   'Australia',
   'Spain',
   'the digital divide',
   'bed',
+  seq(
+    choice('their', 'our', 'the', 'your', 'America\'s'),
+    choice('past', 'future')),
   'the spirit realm',
   'other people\'s business',
   'dentistry',
