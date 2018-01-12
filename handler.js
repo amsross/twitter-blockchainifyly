2add5428fc1edb4fcbe8ef966a44c3943f24f918
Matt Ross <amsross@gmail.com>
2018-01-11 22:10:05 -0500

    feat: respond with text rather than json

--- handler.js
+++ handler.js
@@ -3,10 +3,5 @@ const { run } = require('apep')
 const lib = require('./lib')
 
 module.exports.tweet = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: run(lib)
-  }
-
-  callback(null, response)
+  callback(null, run(lib))
 }
