7e257ced32414e15600af549622bfd75d27b1578
Matt Ross <amsross@gmail.com>
2018-01-11 22:46:12 -0500

    feat: add posting of tweet from handler

--- handler.js
+++ handler.js
@@ -1,7 +1,18 @@
 'use strict'
+const Twitter = require('twitter')
 const { run } = require('apep')
 const lib = require('./lib')
 
+const twitter = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+})
+
 module.exports.tweet = (event, context, callback) => {
-  callback(null, run(lib))
+  twitter.post('statuses/update', { status: run(lib) }, (error, tweet, response) => {
+    if (error) console.error(error)
+    callback(error, tweet && tweet.text)
+  })
 }
--- handler.spec.js
+++ handler.spec.js
@@ -1,7 +1,12 @@
 const test = require('tape')
+const nock = require('nock')
 
 test('handler', assert => {
   const handler = require('./handler')
+
+  nock('https://api.twitter.com').post(/.*/).reply(200, {
+    'text': 'Maybe he\'ll finally find his keys.'
+  })
 
   handler.tweet(null, null, (err, string) => {
     assert.ifError(err, 'no error')
